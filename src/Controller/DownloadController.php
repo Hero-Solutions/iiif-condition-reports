3455bfc07b2fa0fc1797b519964eef1eb01af265
Hero Solutions <info@herosolutions.be>
2023-09-20 15:41:30 +0200

    Gives alert when file upload gives error, imposes 10MB file limit on URL downloads

--- src/Controller/DownloadController.php
+++ src/Controller/DownloadController.php
@@ -8,9 +8,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DownloadController extends AbstractController
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     /**
      * @Route("/{_locale}/download", name="download")
      */
@@ -44,31 +52,55 @@ class DownloadController extends AbstractController
             IMAGETYPE_ICO => "ico"
         );
 
-        if(array_key_exists($type, $mimes)) {
-            $extension = $mimes[$type];
-            $folder = 'annotation_images';
-            $filenameNoExt = round(microtime(true) * 1000);
-            $filename = $folder . '/' . $filenameNoExt . '.' . $extension;
-            $thumbnail = $folder . '/' . $filenameNoExt . '_thm.jpg';
-            copy($image, $filename);
-
-            $thumbnail = IIIFUtil::generateThumbnail($filename, $thumbnail);
-
-            $image = new Image();
-            $image->setImage('/' . $filename);
-            $image->setThumbnail('/' . $thumbnail);
-            $em = $this->container->get('doctrine')->getManager();
-            $em->getConnection()->getConfiguration()->setSQLLogger(null);
-            $em->persist($image);
-            $em->flush();
-
-            $response = new Response(json_encode(array('hash' => $image->getHash(), 'image' => '/' . $filename, 'thumbnail' => '/' . $thumbnail)));
-            $response->headers->set('Content-Type', 'application/json');
-            return $response;
-        } else {
+        if(!array_key_exists($type, $mimes)) {
             $response = new Response(json_encode(array('hash' => null)));
             $response->headers->set('Content-Type', 'application/json');
             return $response;
+        } else {
+            stream_context_set_default(array('http' => array('method' => 'HEAD')));
+            $headers = get_headers($image, 1);
+            stream_context_set_default(array('http' => array('method' => 'GET')));
+
+            if (!isset($headers["Content-Length"])) {
+                $response = new Response(json_encode(array('hash' => null, 'error' => $this->translator->trans('Unable to retrieve file from URL'))));
+                $response->headers->set('Content-Type', 'application/json');
+                return $response;
+            } else {
+                $fileSize = $headers["Content-Length"];
+                if($fileSize >= 10000000) {
+                    $response = new Response(json_encode(array('hash' => null, 'error' => $this->translator->trans('File is too big') . ' (' . $this->formatBytes($fileSize) . ')')));
+                    $response->headers->set('Content-Type', 'application/json');
+                    return $response;
+                } else {
+                    $extension = $mimes[$type];
+                    $folder = 'annotation_images';
+                    $filenameNoExt = round(microtime(true) * 1000);
+                    $filename = $folder . '/' . $filenameNoExt . '.' . $extension;
+                    $thumbnail = $folder . '/' . $filenameNoExt . '_thm.jpg';
+                    copy($image, $filename);
+
+                    $thumbnail = IIIFUtil::generateThumbnail($filename, $thumbnail);
+
+                    $image = new Image();
+                    $image->setImage('/' . $filename);
+                    $image->setThumbnail('/' . $thumbnail);
+                    $em = $this->container->get('doctrine')->getManager();
+                    $em->getConnection()->getConfiguration()->setSQLLogger(null);
+                    $em->persist($image);
+                    $em->flush();
+
+                    $response = new Response(json_encode(array('hash' => $image->getHash(), 'image' => '/' . $filename, 'thumbnail' => '/' . $thumbnail)));
+                    $response->headers->set('Content-Type', 'application/json');
+                    return $response;
+                }
+            }
         }
+    }
+
+    private function formatBytes($size, $precision = 2)
+    {
+        $base = log($size, 1024);
+        $suffixes = array('', 'KB', 'MB', 'GB', 'TB');
+        return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
     }
 }
