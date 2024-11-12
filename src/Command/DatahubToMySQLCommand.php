c7b1def5b365072e1cfdba618fd574c7c591631d
Hero Solutions <info@herosolutions.be>
2024-11-12 17:48:45 +0100

    Removes unused import

--- src/Command/DatahubToMySQLCommand.php
+++ src/Command/DatahubToMySQLCommand.php
@@ -24,7 +24,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class DatahubToMySQLCommand extends Command implements LoggerAwareInterface
